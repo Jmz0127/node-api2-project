45d55d4f3434c2ea3e7bc23c84361406ce0d3161
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-11 20:15:06 -0500

    get request by id tests pass

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get('/');
 
+//GET can get the correct number of posts, can get all the correct posts (4 ms)
 router.get('/', (req, res) => {
 	Post.find()
 		.then((posts) => {
@@ -13,7 +14,25 @@ router.get('/', (req, res) => {
 		.catch((error) => {
 			console.log(error);
 			res.status(500).json({
-				message: 'Error retrieving the posts'
+				message: 'The posts information could not be retrieved'
+			});
+		});
+});
+
+//GET can get all the correct posts by id, responds with a 404 if the post is not found
+router.get('/:id', (req, res) => {
+	Post.findById(req.params.id)
+		.then((posts) => {
+			if (posts) {
+				res.status(200).json(posts);
+			} else {
+				res.status(404).json({ message: 'The post with the specified ID does not exist' });
+			}
+		})
+		.catch((error) => {
+			console.log(error);
+			res.status(500).json({
+				message: 'Error retrieving the post'
 			});
 		});
 });
