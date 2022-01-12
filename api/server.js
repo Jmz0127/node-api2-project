039667382d879eee3a687d63106b6460f735a44b
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-11 19:56:05 -0500

    get request to get all of the correct posts passes

--- api/server.js
+++ api/server.js
@@ -2,7 +2,18 @@
 const express = require('express');
 const server = express();
 
+const postsRouter = require('./posts/posts-router');
+
 server.use(express.json());
+
+server.use('/api/posts', postsRouter);
+
+server.get('/', (req, res) => {
+	res.send(`
+      <h2>Post API</h>
+      <p>Welcome to the Post API</p>
+    `);
+});
 
 // require your posts router and connect it here
 
